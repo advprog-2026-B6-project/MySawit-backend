0b3736ad78066c68aa6c8cdff43e86bc9693ba5c
daffa i <daffa2.ismail178@gmail.com>
2026-02-20 20:06:32 +0700

    dummy endpoint

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/TestController.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.mysawit;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/test-json")
+    public Map<String, String> testJson() {
+        return Map.of("text", "Hello from backend");
+    }
+}
