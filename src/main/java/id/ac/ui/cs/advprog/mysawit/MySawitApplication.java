4149e894d5352b708cc8eda50ec183295ed4c9b1
felesiaj <felesiaj26@gmail.com>
2026-02-20 13:24:40 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/MySawitApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mysawit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySawitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySawitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/mysawit/MySawitApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mysawit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySawitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
