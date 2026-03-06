9f2d28991007b1e649887ceb0e33da28112d5965
felesiaj <felesiaj26@gmail.com>
2026-03-06 13:31:27 +0700

    feat: add unique identifier to KebunSawit model

--- src/main/java/id/ac/ui/cs/advprog/mysawit/kebun/model/KebunSawit.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/kebun/model/KebunSawit.java
@@ -11,6 +11,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class KebunSawit {
+    private String id;
     private String namaKebun;
     private String kodeUnik;
     private double luasHektare;
