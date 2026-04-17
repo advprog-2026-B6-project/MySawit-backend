2ea47537e7f3499f761afd8bc5406540308d9e5c
daffa i <daffa2.ismail178@gmail.com>
2026-04-17 20:40:13 +0700

    idk what this is

--- src/main/java/id/ac/ui/cs/advprog/mysawit/hasil/dto/HasilTodayResponse.java
+++
@@ -1,5 +0,0 @@
-package id.ac.ui.cs.advprog.mysawit.hasil.dto;
-
-public record HasilTodayResponse(boolean hasSubmittedToday, boolean formLocked, String message) {
-}
-
--- src/main/java/id/ac/ui/cs/advprog/mysawit/hasil/exception/DailySubmissionLimitException.java
+++
@@ -1,8 +0,0 @@
-package id.ac.ui.cs.advprog.mysawit.hasil.exception;
-
-public class DailySubmissionLimitException extends RuntimeException {
-    public DailySubmissionLimitException(String message) {
-        super(message);
-    }
-}
-
