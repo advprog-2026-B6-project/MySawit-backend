88a23e4d02c25cf41982fde55c16bb3eed55814d
felesiaj <felesiaj26@gmail.com>
2026-04-17 12:14:02 +0700

    feat: add deletion support in repository layer

--- src/main/java/id/ac/ui/cs/advprog/mysawit/kebun/repository/KebunSawitRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/kebun/repository/KebunSawitRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 
 public interface KebunSawitRepository {
     KebunSawit save(KebunSawit kebun);
+    Optional<KebunSawit> findById(String id);
     Optional<KebunSawit> findByKodeUnik(String kodeUnik);
     List<KebunSawit> findAll();
+    void deleteById(String id);
 }
