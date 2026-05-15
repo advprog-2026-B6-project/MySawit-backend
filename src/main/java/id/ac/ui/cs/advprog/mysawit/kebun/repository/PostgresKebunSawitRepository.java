c9d1f72122a436177365f41f2665a3485f8941f1
felesiaj <felesiaj26@gmail.com>
2026-05-15 14:24:49 +0700

    [REFACTOR] Scope in-memory kebun repository to tests

--- src/main/java/id/ac/ui/cs/advprog/mysawit/kebun/repository/PostgresKebunSawitRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mysawit/kebun/repository/PostgresKebunSawitRepository.java
@@ -1,14 +1,14 @@
 package id.ac.ui.cs.advprog.mysawit.kebun.repository;
 
 import id.ac.ui.cs.advprog.mysawit.kebun.model.KebunSawit;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Primary
+@Profile("!test")
 public class PostgresKebunSawitRepository implements KebunSawitRepository {
 
     private final KebunSawitJpaRepository jpaRepository;
